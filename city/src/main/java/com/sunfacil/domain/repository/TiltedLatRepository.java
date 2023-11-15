e193de01dc503de3610b5c3297f52c09d0a4d51a
maxfideles <maxfideles24@gmail.com>
2023-11-15 02:58:21 -0300

    Tilted Irradiation

---
+++ city/src/main/java/com/sunfacil/domain/repository/TiltedLatRepository.java
@@ -0,0 +1,7 @@
+package com.sunfacil.domain.repository;
+
+import com.sunfacil.domain.model.TiltedLat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TiltedLatRepository extends JpaRepository<TiltedLat,Long> {
+}
